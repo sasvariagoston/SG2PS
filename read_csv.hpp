a2498882f6bab0e6fab4b88b2a4b83d2dce54d7b
Ali Baharev <ali.baharev@gmail.com>
2013-01-11 11:41:49 +0100

    Factored CSV reading out

---
+++ read_csv.hpp
@@ -0,0 +1,13 @@
+// Copyright (C) 2012, Ali Baharev
+// All rights reserved.
+// This code is published under the GNU Lesser General Public License.
+#ifndef READ_CSV_HPP
+#define READ_CSV_HPP
+
+#include <string>
+#include <vector>
+
+// Returns the number of lines read ( == table.size() )
+std::size_t read_csv(const std::string& file_name, std::vector<std::vector<std::string> >& table);
+
+#endif
