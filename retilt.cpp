2d8bf8cfe7af66ba3a0bf662eacb6d5b07551f65
baharev <ali.baharev@gmail.com>
2015-10-03 23:21:39 +0200

    Apparently some character encoding issue

--- retilt.cpp
+++ retilt.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2012-2015, Ã�goston SasvÃ¡ri
+// Copyright (C) 2012-2015, Ágoston Sasvári
 // All rights reserved.
 // This code is published under the GNU Lesser General Public License.
 
